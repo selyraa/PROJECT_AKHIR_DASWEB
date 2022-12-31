12a156e826ac7a9bc219890f7491d5a3b5287ccc
ulfiizza <93421859+ulfiizza@users.noreply.github.com>
2022-12-31 15:03:53 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php   
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "gorent";
-
-    $connect = mysqli_connect($hostname, $username, $password, $database) or die(mysqli_error($connect));
-?>
